ce85041738a4fb59e3e267e898cf02780f14b7d2
Humberto <humberto@Humbertos-MacBook-Air.local>
2023-02-07 21:59:02 +0100

    create util file, add funcHand.

---
+++ pkg/storyUtils/storyUtils.go
@@ -0,0 +1,21 @@
+package storyutils
+
+import "net/http"
+
+func IntroHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func NewYorkHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func DebateHandler(w http.ResponseWriter, r *http.Request) {
+}
+func SeanKellyHandler(w http.ResponseWriter, r *http.Request) {
+}
+func MarkBatesHandler(w http.ResponseWriter, r *http.Request) {
+}
+func DenverHandler(w http.ResponseWriter, r *http.Request) {
+}
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+}
