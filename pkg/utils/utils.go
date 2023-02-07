2a55248226b405c84dcc058bf90e1d17c8afdd39
Humberto <humberto@Humbertos-MacBook-Air.local>
2023-02-07 21:59:34 +0100

    parse JSON and return map

---
+++ pkg/utils/utils.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/humberto1212/gopher-story/pkg/models"
+)
+
+func ParseJSON() map[string]models.StoryArc {
+
+	// Open jsonFile
+	jsonFile, err := os.Open("/Users/humberto/go/src/github.com/humberto1212/gopher-exer/gopher-story/story.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	// read our opened jsonFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// Initialize histories array
+	//var stories models.Histories
+
+	var result = map[string]models.StoryArc{}
+
+	json.Unmarshal([]byte(byteValue), &result)
+
+	return result
+}
