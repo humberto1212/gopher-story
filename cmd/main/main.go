b5a77f78b77f35259b841589ca21fbc15bac9a78
Humberto <humberto@Humbertos-MacBook-Air.local>
2023-02-10 19:04:36 +0100

    add debate, update file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	storyutils "github.com/humberto1212/gopher-story/pkg/storyUtils"
-	"github.com/humberto1212/gopher-story/pkg/utils"
 )
 
 func main() {
 
-	history := utils.ParseJSON()
-
-	fmt.Println(history)
-
 	r := mux.NewRouter()
-	r.HandleFunc("/intro", storyutils.IntroHandler).Methods("GET")
+	r.HandleFunc("/", storyutils.IntroHandler).Methods("GET")
 	r.HandleFunc("/new-york", storyutils.NewYorkHandler).Methods("GET")
+	r.HandleFunc("/debate", storyutils.DebateHandler).Methods("GET")
 	r.HandleFunc("/sean-kelly", storyutils.SeanKellyHandler).Methods("GET")
 	r.HandleFunc("/mark-bates", storyutils.MarkBatesHandler).Methods("GET")
 	r.HandleFunc("/denver", storyutils.DenverHandler).Methods("GET")
